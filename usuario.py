2a73ffba6ce434feca8ca4b873d0daeb66b0b106
Juan Diego Lozano Luna <judlozanol@udistrital.edu.co>
2023-10-20 16:29:23 -0500

    eliminacion duplicados

--- usuario.py
+++
@@ -1,9 +0,0 @@
-class Usuario():
-    def __init__(self):
-        self.cargo: type[str]
-        self.sede: type[str]
-        self.carrera: type[str]
-        self.nombre: type[str]
-        self.documento: type[int]
-    def llenar_usuario(self):
-        pass
